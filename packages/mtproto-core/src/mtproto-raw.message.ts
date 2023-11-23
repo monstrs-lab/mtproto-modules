e529eef9e03a5eeffcd88aa3a5773c971ce8602c
Andrey Linko <AndreyLinko@gmail.com>
2023-11-23 16:11:25 +0300

    Revert "feat: change signed to unsigned"

--- packages/mtproto-core/src/mtproto-raw.message.ts
+++ packages/mtproto-core/src/mtproto-raw.message.ts
@@ -20,7 +20,7 @@ export class MTProtoRawMessage {
     payload: Buffer,
     context: MTProtoRawMessageContext
   ): Promise<MTProtoRawMessage> {
-    const authKeyId = payload.readBigUInt64BE(0)
+    const authKeyId = payload.readBigInt64LE(0)
 
     if (authKeyId === 0n) {
       return new MTProtoRawMessage(
