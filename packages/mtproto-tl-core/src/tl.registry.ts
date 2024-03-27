a233197ffcf48c8949d03c2fecc25edce65d7921
Andrey Linko <AndreyLinko@gmail.com>
2024-03-27 16:40:24 +0300

    feat: tl to typescript async registry

--- packages/mtproto-tl-core/src/tl.registry.ts
+++ packages/mtproto-tl-core/src/tl.registry.ts
@@ -3,17 +3,17 @@ import type { BytesIO }  from '@tgsnake/core/lib/src/raw/core/BytesIO.js'
 import type { TLObject } from './tl.object.js'
 
 export class TLRegistry {
-  constructor(private readonly mapping: Map<number, typeof TLObject>) {}
+  constructor(private readonly mapping: Map<number, () => Promise<typeof TLObject>>) {}
 
   async read<T extends TLObject>(data: BytesIO): Promise<T> {
     const id = data.readUInt32LE(4)
 
-    const clazz = this.mapping.get(id)
+    const clazz = await this.mapping.get(id)
 
     if (!clazz) {
       throw new Error(`Registry mapping for ${id} not found`)
     }
 
-    return clazz.read(data, this) as Promise<T>
+    return (await clazz()).read(data, this) as Promise<T>
   }
 }
